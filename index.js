a839410162c0238c8312585c69940929909a0cad
James <james.ekasiba@student.moringaschool.com>
2023-10-11 22:47:23 -0700

    Fetcing Public API

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+  fetchDogBreedDetails();
 });
+
+function fetchDogBreedDetails(energy = "3") {
+  const myHeaders = new Headers();
+  myHeaders.append("X-Api-Key", "2ujXDP62s+wRKQKcy/7fng==PsgZKBri0ByBkKtn");
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+  };
+
+  fetch(`https://api.api-ninjas.com/v1/dogs?energy=${energy}`, requestOptions)
+    .then((response) => response.json())
+    .then((result) => renderBreeds(result));
+}
