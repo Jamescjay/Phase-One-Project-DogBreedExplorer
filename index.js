f0b60427584b9f2c4abb66cea969429bef3cb55a
James <james.ekasiba@student.moringaschool.com>
2023-10-13 16:30:47 -0700

    Adding comments

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+
+//Manipulating the DOM
 document.addEventListener("DOMContentLoaded", () => {
-  
-   const breedSearchInput = document.getElementById("breedSearch");
-   breedSearchInput.addEventListener("input", searchBreeds);
-  
+  // Add an event listener to the "input" event for the breed search input, which triggers the "searchBreeds" function
+  const breedSearchInput = document.getElementById("breedSearch");
+  breedSearchInput.addEventListener("input", searchBreeds);
+
+  // Fetch dog breed details from the public API
   fetchDogBreedDetails();
 });
 
 let allBreeds = [];
 
+// Function to fetch dog breed details, with a default energy level of "3"
 function fetchDogBreedDetails(energy = "3") {
   const myHeaders = new Headers();
   myHeaders.append("X-Api-Key", "2ujXDP62s+wRKQKcy/7fng==PsgZKBri0ByBkKtn");
@@ -18,17 +22,21 @@ function fetchDogBreedDetails(energy = "3") {
   };
 
   fetch(`https://api.api-ninjas.com/v1/dogs?energy=${energy}`, requestOptions)
-    .then((response) => response.json())
+    .then((response) => response.json()) // Parse the response as JSON
     .then((result) => {
-      allBreeds =result
-      renderBreeds(allBreeds)
+      allBreeds = result; // Store the retrieved dog breed details in the "allBreeds" variable
+      renderBreeds(allBreeds); // Render the list of dog breeds
     });
 }
 
+// Function to render the list of dog breeds
 function renderBreeds(dogs) {
   const dogList = document.getElementById("breeds");
+
+  // Clear the previous content of the dog list
   dogList.innerHTML = "";
 
+  // Loop through the dog data and create a card for each dog
   dogs.forEach((dog) => {
     const parentDiv = document.createElement("div");
     parentDiv.classList.add("col");
@@ -51,10 +59,10 @@ function renderBreeds(dogs) {
 
     cardDiv.appendChild(cardBody);
 
-
     parentDiv.appendChild(cardDiv);
     dogList.appendChild(parentDiv);
 
+    // Add a click event listener to each card to display dog details when a card is clicked
     cardDiv.addEventListener("click", () => {
       displayDogDetails(dog);
       hideDogCards();
@@ -62,44 +70,49 @@ function renderBreeds(dogs) {
   });
 }
 
-function displayDogDetails(dog) {
 
+// Function to display dog details
+function displayDogDetails(dog) {
+  // Display the dog details container as a block
   const detailsDiv = document.getElementById("details");
   detailsDiv.style.display = "block";
 
-   document.getElementById("dogImage").src = dog.image_link;
-   document.getElementById("dogName").textContent = dog.name;
-   document.getElementById("dogGrooming").textContent = dog.grooming;
-   document.getElementById("strangerRel").textContent = dog.good_with_strangers;
-   document.getElementById("dogPlayfulness").textContent = dog.playfulness;
-   document.getElementById("dogTrainability").textContent = dog.trainability;
-   document.getElementById("dogEnergy").textContent = dog.energy;
-   document.getElementById("dogBarking").textContent = dog.barking;
-   document.getElementById("maxMale").textContent = dog.max_weight_male;
-   document.getElementById("maxFemale").textContent = dog.max_weight_female;
-   document.getElementById("minMale").textContent = dog.min_weight_male;
-   document.getElementById("minFemale").textContent = dog.min_weight_female;
+  // Set various details about the dog using DOM manipulation and accessing them in the fetched API
+  document.getElementById("dogImage").src = dog.image_link;
+  document.getElementById("dogName").textContent = dog.name;
+  document.getElementById("dogGrooming").textContent = dog.grooming;
+  document.getElementById("strangerRel").textContent = dog.good_with_strangers;
+  document.getElementById("dogPlayfulness").textContent = dog.playfulness;
+  document.getElementById("dogTrainability").textContent = dog.trainability;
+  document.getElementById("dogEnergy").textContent = dog.energy;
+  document.getElementById("dogBarking").textContent = dog.barking;
+  document.getElementById("maxMale").textContent = dog.max_weight_male;
+  document.getElementById("maxFemale").textContent = dog.max_weight_female;
+  document.getElementById("minMale").textContent = dog.min_weight_male;
+  document.getElementById("minFemale").textContent = dog.min_weight_female;
 }
 
+// Function to hide the list of dog cards and other elements in the div by class name container
 function hideDogCards() {
   const container = document.querySelector(".container");
-  container.style.display = "none";
+  container.style.display = "none"; //applying a style to display none
 }
 
-
+// Function to search and filter dog breeds
 function searchBreeds() {
   const searchQuery = document
     .getElementById("breedSearch")
-    .value.toLowerCase();
+    .value.toLowerCase(); // Get the search query from the input field
 
+  // Filter the list of dog breeds based on the search query
   const filteredBreeds = allBreeds.filter((dog) =>
     dog.name.toLowerCase().includes(searchQuery)
   );
 
   if (filteredBreeds.length === 0) {
-    alert(" Error!!! Breed not found, please try another one.");
+    alert(" Error!!! Breed not found, please try another one."); // Show this alert message if no matching breed is found
   }
 
-renderBreeds(filteredBreeds);
-
+  // Render the filtered list of dog breeds
+  renderBreeds(filteredBreeds);
 }
